8e49614bd10e2e859a035d926d2f0ae6b0c88c16
吳宣逸 <t80422@hotmail.com>
2023-05-24 11:44:05 +0800

    更新客戶管理

--- frmTaboo.Designer.vb
+++ frmTaboo.Designer.vb
@@ -22,105 +22,60 @@ Partial Class frmTaboo
     '請勿使用程式碼編輯器進行修改。
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.cmbType = New System.Windows.Forms.ComboBox()
+        Me.flpMain = New System.Windows.Forms.FlowLayoutPanel()
+        Me.btnOK = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'ComboBox1
+        'cmbType
         '
-        Me.ComboBox1.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(12, 12)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 27)
-        Me.ComboBox1.TabIndex = 0
-        Me.ComboBox1.Text = "全部"
+        Me.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbType.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.cmbType.FormattingEnabled = True
+        Me.cmbType.Location = New System.Drawing.Point(12, 12)
+        Me.cmbType.Name = "cmbType"
+        Me.cmbType.Size = New System.Drawing.Size(121, 27)
+        Me.cmbType.TabIndex = 0
         '
-        'FlowLayoutPanel1
+        'flpMain
         '
-        Me.FlowLayoutPanel1.AutoScroll = True
-        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.White
-        Me.FlowLayoutPanel1.Controls.Add(Me.CheckBox1)
-        Me.FlowLayoutPanel1.Controls.Add(Me.CheckBox2)
-        Me.FlowLayoutPanel1.Controls.Add(Me.CheckBox3)
-        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(12, 44)
-        Me.FlowLayoutPanel1.Margin = New System.Windows.Forms.Padding(0)
-        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(250, 261)
-        Me.FlowLayoutPanel1.TabIndex = 1
+        Me.flpMain.AutoScroll = True
+        Me.flpMain.BackColor = System.Drawing.Color.White
+        Me.flpMain.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.flpMain.Location = New System.Drawing.Point(12, 44)
+        Me.flpMain.Margin = New System.Windows.Forms.Padding(0)
+        Me.flpMain.Name = "flpMain"
+        Me.flpMain.Size = New System.Drawing.Size(250, 261)
+        Me.flpMain.TabIndex = 1
         '
-        'Button1
+        'btnOK
         '
-        Me.Button1.AutoSize = True
-        Me.Button1.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.Button1.Location = New System.Drawing.Point(185, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 29)
-        Me.Button1.TabIndex = 20
-        Me.Button1.Text = "確定"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'CheckBox3
-        '
-        Me.CheckBox3.AutoSize = True
-        Me.CheckBox3.Font = New System.Drawing.Font("標楷體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.CheckBox3.Location = New System.Drawing.Point(3, 55)
-        Me.CheckBox3.Name = "CheckBox3"
-        Me.CheckBox3.Size = New System.Drawing.Size(59, 20)
-        Me.CheckBox3.TabIndex = 2
-        Me.CheckBox3.Text = "花生"
-        Me.CheckBox3.UseVisualStyleBackColor = True
-        '
-        'CheckBox2
-        '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Font = New System.Drawing.Font("標楷體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.CheckBox2.Location = New System.Drawing.Point(3, 29)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(43, 20)
-        Me.CheckBox2.TabIndex = 1
-        Me.CheckBox2.Text = "蝦"
-        Me.CheckBox2.UseVisualStyleBackColor = True
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Font = New System.Drawing.Font("標楷體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(3, 3)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(59, 20)
-        Me.CheckBox1.TabIndex = 0
-        Me.CheckBox1.Text = "豬肉"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.btnOK.AutoSize = True
+        Me.btnOK.Font = New System.Drawing.Font("標楷體", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.btnOK.Location = New System.Drawing.Point(185, 12)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 29)
+        Me.btnOK.TabIndex = 20
+        Me.btnOK.Text = "確定"
+        Me.btnOK.UseVisualStyleBackColor = True
         '
         'frmTaboo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(272, 314)
-        Me.Controls.Add(Me.FlowLayoutPanel1)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.flpMain)
+        Me.Controls.Add(Me.cmbType)
+        Me.Controls.Add(Me.btnOK)
         Me.Name = "frmTaboo"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "禁忌清單"
-        Me.FlowLayoutPanel1.ResumeLayout(False)
-        Me.FlowLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
-    Friend WithEvents Button1 As Button
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
-    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents cmbType As ComboBox
+    Friend WithEvents flpMain As FlowLayoutPanel
+    Friend WithEvents btnOK As Button
 End Class
---
+++ modMySQL.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Configuration
+Imports MySql.Data.MySqlClient
+
+Module modMySQL
+    Friend mConn As MySqlConnection
+
+    Friend Sub InitMySQL()
+        '設定連線
+        Dim myConnectionString As String = ConfigurationManager.AppSettings("myConnectionString").ToString
+        mConn = New MySqlConnection(myConnectionString)
+    End Sub
+    ''' <summary>
+    ''' 查詢資料表
+    ''' </summary>
+    ''' <returns></returns>
+    Friend Function SelectFromTable(sSQL As String) As DataTable
+        Dim dt As New DataTable()
+        Try
+            mConn.Open()
+            Using cmd As New MySqlCommand(sSQL, mConn)
+                Dim adapter As New MySqlDataAdapter(cmd)
+                adapter.Fill(dt)
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        mConn.Close()
+        Return dt
+    End Function
+End Module
